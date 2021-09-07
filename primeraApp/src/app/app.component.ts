6a17b6fdbe86216a5db1e1e89c9064e3cf4ce2b0
Nicolas <nfonseca1999@gmail.com>
2021-09-07 07:49:41 -0500

    Creación calculadora

--- primeraApp/src/app/app.component.ts
+++ primeraApp/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeraApp';
+  numero1: number=0;
+  numero2: number=0;
+  resultado:number =0;
+  public operar(op: string):void {
+    switch(op){
+      case 's': 
+      this.resultado = this.numero1 + this.numero2;
+      break;
+      case 'r':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'm':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case 'd':
+        this.resultado = this.numero1 / this.numero2;
+        break;
+      default:
+        this.resultado=0;
+        
+
+    }
+
+  }
+
+
 }
